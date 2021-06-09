d85f2fd95af31165a0008c0f00f4cf9b89c70088
Jens Bouma <jensmbouma@kpnmail.nl>
2021-06-09 11:29:14 +0200

    Website gemaakt voor de tegeltjes

--- Web/routes/web.php
+++ Web/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{all}', function () {
     return view('welcome');
+})->where('all', '^(?!admin|dashboard|login|register|logout|home|user).*$');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
