afe161cb9877914e2ef76ec488c59763985a6db6
weshakvoort <70153169+weshakvoort@users.noreply.github.com>
2021-07-07 11:56:38 +0200

    ben dom

--- Mobile/App.config.js
+++ Mobile/App.config.js
@@ -1,5 +1,5 @@
 /**
- * Tussen de twee "kan je de link in plakken van de API"
+ * Tussen de twee "" kan je de link in plakken van de API
  * @type {string}
  */
 export const config = module.exports = "http://10.0.2.2:8000";
